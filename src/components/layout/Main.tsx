26657b468763b2406401ea1595e442423d6b629f
d9m0n4 <c2h8e9d4@gmail.com>
2022-10-24 16:51:20 +0300

    sliders buttons

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -15,7 +15,7 @@ import CustomNextArrow from '../ui/Slider/arrows/nextArrow';
 import CustomPrevArrow from '../ui/Slider/arrows/prevArrow';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
+import { Navigation, Pagination, Swiper as SwiperType } from 'swiper';
 
 import 'swiper/css';
 
@@ -25,26 +25,28 @@ const Main = () => {
   // const { data, error, isLoading } = useGetVideosQuery({ id: 1108401 });
   // const { data } = useGetImagesQuery({ id: 1108401 });
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    nextArrow: <CustomNextArrow />,
-    prevArrow: <CustomPrevArrow />,
-    touchMove: false,
-  };
-  const settings2 = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    nextArrow: <CustomNextArrow />,
-    prevArrow: <CustomPrevArrow />,
-    touchMove: false,
-  };
+  // const settings = {
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 6,
+  //   slidesToScroll: 6,
+  //   nextArrow: <CustomNextArrow />,
+  //   prevArrow: <CustomPrevArrow />,
+  //   touchMove: false,
+  // };
+  // const settings2 = {
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 4,
+  //   nextArrow: <CustomNextArrow />,
+  //   prevArrow: <CustomPrevArrow />,
+  //   touchMove: false,
+  // };
+
+  const swiperRef = React.useRef<SwiperType>();
 
   return (
     <>
@@ -62,30 +64,39 @@ const Main = () => {
             <article>
               <h3>Рекомендуем посмотреть</h3>
               <div className="mt-4">
-                <Swiper slidesPerView={6} loop modules={[Navigation]}>
-                  {data &&
-                    data.films.map((film: any) => (
-                      <SwiperSlide key={film.filmId} className=" p-2 ">
-                        <Link to={`/film/${film.filmId}`}>
-                          <div className="relative">
-                            <img src={film.posterUrlPreview} alt="1" className="rounded-md " />
-                            <div className="absolute  inset-0  opacity-0 hover:opacity-100 transition-all">
-                              <div className="inline-block py-2 px-4 bg-accentPurple rounded-lg">
-                                <div>{film.rating}</div>
-                              </div>
-                              <div className="absolute bottom-0 left-0 right-0 flex items-center justify-between py-2 px-4 text-sm font-normal bg-accentWrapper">
-                                <span>{film.year}</span>
-                                <span>{film.countries[0].country}</span>
+                <div className="relative">
+                  <Swiper
+                    slidesPerView={6}
+                    slidesPerGroup={6}
+                    loop
+                    onBeforeInit={(swiper) => (swiperRef.current = swiper)}
+                    allowTouchMove={false}>
+                    {data &&
+                      data.films.map((film: any) => (
+                        <SwiperSlide key={film.filmId} className="p-2 ">
+                          <Link to={`/film/${film.filmId}`}>
+                            <div className="relative">
+                              <img src={film.posterUrlPreview} alt="1" className="rounded-md " />
+                              <div className="absolute  inset-0  opacity-0 hover:opacity-100 transition-all">
+                                <div className="inline-block py-2 px-4 bg-accentPurple rounded-lg">
+                                  <div>{film.rating}</div>
+                                </div>
+                                <div className="absolute bottom-0 left-0 right-0 flex items-center justify-between py-2 px-4 text-sm font-normal bg-accentWrapper">
+                                  <span>{film.year}</span>
+                                  <span>{film.countries[0].country}</span>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                          <div className="text-ellipsis overflow-hidden whitespace-nowrap">
-                            <span className="text-sm mt-2 font-normal"> {film.nameRu}</span>
-                          </div>
-                        </Link>
-                      </SwiperSlide>
-                    ))}
-                </Swiper>
+                            <div className="text-ellipsis overflow-hidden whitespace-nowrap">
+                              <span className="text-sm mt-2 font-normal"> {film.nameRu}</span>
+                            </div>
+                          </Link>
+                        </SwiperSlide>
+                      ))}
+                  </Swiper>
+                  <CustomPrevArrow swiperRef={swiperRef} className="absolute -left-2.5 top-0" />
+                  <CustomNextArrow swiperRef={swiperRef} className="absolute -right-2.5 top-0" />
+                </div>
 
                 {/* <Slider {...settings}>
                   {data &&
@@ -118,7 +129,7 @@ const Main = () => {
             <article>
               <h3>Рекомендуем посмотреть</h3>
               <div className="mt-4">
-                <Slider {...settings2}>
+                {/* <Slider {...settings2}>
                   {data &&
                     data.films.map((film: any) => (
                       <div key={film.filmId} className=" p-2 ">
@@ -141,7 +152,7 @@ const Main = () => {
                         </Link>
                       </div>
                     ))}
-                </Slider>
+                </Slider> */}
               </div>
             </article>
           </div>
--- src/components/ui/Slider/arrows/prevArrow.tsx
+++ src/components/ui/Slider/arrows/prevArrow.tsx
@@ -1,30 +1,38 @@
 import clsx from 'clsx';
 import React from 'react';
+import Swiper from 'swiper';
 
-const CustomPrevArrow = (props: any) => {
-  const { className, style, onClick } = props;
+interface ISwiperNavBtn {
+  swiperRef: React.RefObject<Swiper | undefined> | undefined;
+  className?: string;
+  withShadow?: boolean;
+}
+
+const CustomPrevArrow: React.FC<ISwiperNavBtn> = ({ swiperRef, className, withShadow = true }) => {
   return (
     <div
-      className={clsx(
-        className,
-        'flex items-center justify-center leftArrowGradient transition-all hover:leftArrowGradient h-full w-16 before:content-none z-10',
-      )}
-      onClick={onClick}>
-      <svg
-        className="mr-6"
-        width="12"
-        height="20"
-        viewBox="0 0 12 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M10.3333 18.4002L0.999918 10.1475L10.3333 1.89478"
-          stroke="white"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      className={clsx(className, {
+        'group flex items-center justify-center hover:opacity-100 leftArrowGradient transition-all hover:leftArrowGradient h-full w-16 before:content-none z-10':
+          withShadow,
+      })}>
+      <button
+        className="p-2 -translate-y-1/4 opacity-0 group-hover:opacity-100 transition-opacity"
+        onClick={() => swiperRef?.current?.slidePrev()}>
+        <svg
+          width="12"
+          height="20"
+          viewBox="0 0 12 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M10.3333 18.4002L0.999918 10.1475L10.3333 1.89478"
+            stroke="white"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
