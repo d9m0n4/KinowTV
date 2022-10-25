60b837da0c0d970b37c5cf0b24e0ecf050721cea
d9m0n4 <c2h8e9d4@gmail.com>
2022-10-25 16:51:45 +0300

    vertical slider

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -15,7 +15,7 @@ import CustomNextArrow from '../ui/Slider/arrows/nextArrow';
 import CustomPrevArrow from '../ui/Slider/arrows/prevArrow';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Swiper as SwiperType } from 'swiper';
+import { Autoplay, Navigation, Pagination, Swiper as SwiperType } from 'swiper';
 
 import 'swiper/css';
 
@@ -55,6 +55,52 @@ const Main = () => {
           <img className="object-cover w-full h-full" src={cover} alt="" />
           <div className="absolute inset-0 bg-accentDark opacity-30"></div>
           <div className="absolute coverShadow left-0 top-0 bottom-0 w-1/4"></div>
+          <div className="container mx-auto absolute inset-0">
+            <div className="absolute right-0 top-0 bottom-0 flex">
+              <Swiper
+                lazy={{
+                  loadOnTransitionStart: false,
+                  loadPrevNext: false,
+                }}
+                speed={3500}
+                direction="vertical"
+                modules={[Autoplay]}
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                loop
+                slidesPerView={4}
+                slidesPerGroup={4}
+                className="h-full">
+                {data &&
+                  data.films.map((film: any) => (
+                    <SwiperSlide key={film.filmId} className="p-2 h-full max-w-[140px]">
+                      <Link to={`/film/${film.filmId}`}>
+                        <div className="relative">
+                          <img src={film.posterUrlPreview} alt="1" className="rounded-md " />
+                        </div>
+                      </Link>
+                    </SwiperSlide>
+                  ))}
+              </Swiper>
+              {/* <Swiper
+                direction="vertical"
+                modules={[Autoplay]}
+                autoplay={{ delay: 0, reverseDirection: true, disableOnInteraction: false }}
+                loop
+                slidesPerView={4}
+                className="h-full">
+                {data &&
+                  data.films.map((film: any) => (
+                    <SwiperSlide key={film.filmId} className="p-2 h-full max-w-[140px]">
+                      <Link to={`/film/${film.filmId}`}>
+                        <div className="relative">
+                          <img src={film.posterUrlPreview} alt="1" className="rounded-md " />
+                        </div>
+                      </Link>
+                    </SwiperSlide>
+                  ))}
+              </Swiper> */}
+            </div>
+          </div>
         </div>
       </section>
 
@@ -65,35 +111,41 @@ const Main = () => {
               <h3>Рекомендуем посмотреть</h3>
               <div className="mt-4">
                 <div className="relative">
-                  <Swiper
-                    slidesPerView={6}
-                    slidesPerGroup={6}
-                    loop
-                    onBeforeInit={(swiper) => (swiperRef.current = swiper)}
-                    allowTouchMove={false}>
-                    {data &&
-                      data.films.map((film: any) => (
-                        <SwiperSlide key={film.filmId} className="p-2 ">
-                          <Link to={`/film/${film.filmId}`}>
-                            <div className="relative">
-                              <img src={film.posterUrlPreview} alt="1" className="rounded-md " />
-                              <div className="absolute  inset-0  opacity-0 hover:opacity-100 transition-all">
-                                <div className="inline-block py-2 px-4 bg-accentPurple rounded-lg">
-                                  <div>{film.rating}</div>
-                                </div>
-                                <div className="absolute bottom-0 left-0 right-0 flex items-center justify-between py-2 px-4 text-sm font-normal bg-accentWrapper">
-                                  <span>{film.year}</span>
-                                  <span>{film.countries[0].country}</span>
+                  {data && (
+                    <Swiper
+                      lazy={{
+                        loadOnTransitionStart: false,
+                        loadPrevNext: false,
+                      }}
+                      slidesPerView={6}
+                      slidesPerGroup={6}
+                      loop
+                      onBeforeInit={(swiper) => (swiperRef.current = swiper)}
+                      allowTouchMove={false}>
+                      {data &&
+                        data.films.map((film: any) => (
+                          <SwiperSlide key={film.filmId} className="p-2 ">
+                            <Link to={`/film/${film.filmId}`}>
+                              <div className="relative">
+                                <img src={film.posterUrlPreview} alt="1" className="rounded-md " />
+                                <div className="absolute  inset-0  opacity-0 hover:opacity-100 transition-all">
+                                  <div className="inline-block py-2 px-4 bg-accentPurple rounded-lg">
+                                    <div>{film.rating}</div>
+                                  </div>
+                                  <div className="absolute bottom-0 left-0 right-0 flex items-center justify-between py-2 px-4 text-sm font-normal bg-accentWrapper">
+                                    <span>{film.year}</span>
+                                    <span>{film.countries[0].country}</span>
+                                  </div>
                                 </div>
                               </div>
-                            </div>
-                            <div className="text-ellipsis overflow-hidden whitespace-nowrap">
-                              <span className="text-sm mt-2 font-normal"> {film.nameRu}</span>
-                            </div>
-                          </Link>
-                        </SwiperSlide>
-                      ))}
-                  </Swiper>
+                              <div className="text-ellipsis overflow-hidden whitespace-nowrap">
+                                <span className="text-sm mt-2 font-normal"> {film.nameRu}</span>
+                              </div>
+                            </Link>
+                          </SwiperSlide>
+                        ))}
+                    </Swiper>
+                  )}
                   <CustomPrevArrow swiperRef={swiperRef} className="absolute -left-2.5 top-0" />
                   <CustomNextArrow swiperRef={swiperRef} className="absolute -right-2.5 top-0" />
                 </div>
