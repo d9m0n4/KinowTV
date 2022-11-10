18b3bc20826f15da945bd2101f57ea52dbc9aa31
d9m0n4 <c2h8e9d4@gmail.com>
2022-11-10 16:52:27 +0300

    person page info

--- src/pages/person.tsx
+++ src/pages/person.tsx
@@ -1,7 +1,8 @@
 import { skipToken } from '@reduxjs/toolkit/dist/query';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import PersonAvatar from '../components/shared/PersonAvatar';
+import Button from '../components/ui/Button';
 import { IPerson } from '../models';
 import { useGetStaffByPersonIdQuery } from '../services/moviesAPI';
 import { uniqueObjArray } from '../utils/uniqueObjArray';
@@ -29,84 +30,122 @@ const Person = () => {
                   />
                 </div>
                 <div className="flex-auto px-8 text-secondaryText">
-                  <div className="mb-4">
-                    {data.nameRu ? (
-                      <>
-                        <div className="mb-4 flex">
-                          <h2 className="text-4xl font-bold">{data.nameRu}</h2>
-                        </div>
-                      </>
-                    ) : (
-                      <>
-                        <div className="my-2 flex">
-                          <h2 className="w-48">{data.nameEn}</h2>
-                        </div>
-                      </>
-                    )}
-                    {data.profession && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Карьера:</h6>
-                          <span>{data.profession}</span>
-                        </div>
-                      </>
-                    )}
-                    {data.age && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Возраст:</h6>
-                          <span>{data.age}</span>
-                        </div>
-                      </>
-                    )}
-                    {data.growth && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Рост:</h6>
-                          <span>{data.growth}</span>
-                        </div>
-                      </>
-                    )}
-                    {data.birthday && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Дата рождения:</h6>
-                          <span>{data.birthday}</span>
-                        </div>
-                      </>
-                    )}
-                    {data.birthplace && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Место рождения</h6>
-                          <span>{data.birthplace}</span>
-                        </div>
-                      </>
-                    )}
-                    {data.death && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Дата смерти:</h6>
-                          <span>{data.death}</span>
-                        </div>
-                      </>
-                    )}
-                    {data.deathplace && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Место смерти:</h6>
-                          <span>{data.deathplace}</span>
-                        </div>
-                      </>
-                    )}
-                    {data.films.length > 0 && (
-                      <>
-                        <div className="my-2 flex">
-                          <h6 className="w-48">Всего фильмов:</h6>
-                          <span>{data.films.length}</span>
-                        </div>
-                      </>
-                    )}
+                  <div className="flex flex-col justify-between h-full">
+                    <div className="pb-2">
+                      {data.nameRu ? (
+                        <>
+                          <div className="mb-4 flex">
+                            <h2 className="text-4xl font-bold">{data.nameRu}</h2>
+                          </div>
+                        </>
+                      ) : (
+                        <>
+                          <div className="my-2 flex">
+                            <h2 className="w-48">{data.nameEn}</h2>
+                          </div>
+                        </>
+                      )}
+                      {data.profession && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Карьера:</h6>
+                            <span>{data.profession}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.age && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Возраст:</h6>
+                            <span>{data.age}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.growth && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Рост:</h6>
+                            <span>{data.growth}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.birthday && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Дата рождения:</h6>
+                            <span>{data.birthday}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.birthplace && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Место рождения</h6>
+                            <span>{data.birthplace}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.death && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Дата смерти:</h6>
+                            <span>{data.death}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.deathplace && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Место смерти:</h6>
+                            <span>{data.deathplace}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.films.length > 0 && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Всего фильмов:</h6>
+                            <span>{data.films.length}</span>
+                          </div>
+                        </>
+                      )}
+                      {data.spouses && data.spouses.length > 0 && (
+                        <>
+                          <div className="my-2 flex">
+                            <h6 className="w-48">Супруга:</h6>
+                            <div className="flex flex-col">
+                              {data.spouses.map((spouse) => (
+                                <div>
+                                  <span>{spouse.name}</span>
+                                  {spouse.divorcedReason && <span>{spouse.divorcedReason}</span>}
+                                </div>
+                              ))}
+                            </div>
+                          </div>
+                        </>
+                      )}
+                    </div>
+                    <div className="pb-2">
+                      <Button className="flex items-center  px-8 py-2 bg-accentPurple rounded-lg">
+                        <span className="text-lg">В избранное</span>
+                        <i className="ml-6">
+                          <svg
+                            width="16"
+                            height="20"
+                            viewBox="0 0 16 20"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              d="M1 3C1 1.89543 1.89543 1 3 1H13C14.1046 1 15 1.89543 15 3V19L8 15.5L1 19V3Z"
+                              stroke="white"
+                              strokeWidth="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                        </i>
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -122,11 +161,17 @@ const Person = () => {
 
                     {/* <Tabs /> */}
 
-                    <ul>
+                    <ul className="text-lg my-2">
                       {data.films.map((film) => (
-                        <li className="py-2 flex shadow-gray shadow-md" key={film.filmId}>
-                          <h4>{film.nameRu}</h4>
-                          <span>{film.rating}</span>
+                        <li
+                          className="p-4  shadow-accentWrapper rounded-lg shadow-md hover:shadow-none transition-all"
+                          key={film.filmId}>
+                          <Link to={`/film/${film.filmId}`} className="flex justify-between">
+                            <h4 className="font-medium">{film.nameRu}</h4>
+                            <span className="bg-gray rounded-md py-1 px-2 font-bold text-base block text-accentDark">
+                              {film.rating}
+                            </span>
+                          </Link>
                         </li>
                       ))}
                     </ul>
